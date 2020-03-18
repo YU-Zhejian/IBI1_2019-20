97ca0ac608fdad45ee5050198d3894ad6ed3d2cb
YU Zhejian <Zhejian.19@intl.zju.edu.cn>
2020-03-18 13:19:36 +0800

    Pseudo code added to Practical 6.

--- Pratical6/DNA_Count.py
+++ Pratical6/DNA_Count.py
@@ -1,19 +1,25 @@
 #!/bin/env python
+#Get data
 import matplotlib.pyplot as plt
 dna="ATGCTTCAGAAAGGTCTTACG"
 dna_dict={'A':0,'T':0,'C':0,'G':0}
 
+#Add to dictionary
 for n in range(0,len(dna)):
     dna_dict[dna[n:n+1]]=dna_dict[dna[n:n+1]]+1
 
+#Enumerate dictionary
+for dna_key in dna_dict:
+    print(dna_key+"="+str(dna_dict[dna_key]))
+
+#Transcribe the dictionary to a list, whcih may be used by plt.
 dna_list=[0,0,0,0]
 dna_list[0]=dna_dict['A']
 dna_list[1]=dna_dict['T']
 dna_list[2]=dna_dict['C']
 dna_list[3]=dna_dict['G']
 
-for dna_key in dna_dict:
-    print(dna_key+"="+str(dna_dict[dna_key]))
+#Plot the fata
 lbl='A','T','C','G'
 plt.pie(dna_list,labels=lbl,autopct='%1.1f%%')
 plt.axis('equal')
--- Pratical6/List_sort_filt.py
+++ Pratical6/List_sort_filt.py
@@ -1,10 +1,13 @@
 #!/bin/env python
 import matplotlib.pyplot as plt
+# Get the data
 gene_lengths=[9410,3944141,4442,105338,19149,76779,126550,36296,842,15981]
 
+#Sort and remove head & Tail
 gene_lengths.sort()
 gene_lengths=gene_lengths[1:len(gene_lengths)-1]
 
+#Plot the data
 plt.boxplot(gene_lengths,labels=["Gene"])
 plt.title("Gene length distribution")
 plt.show()
