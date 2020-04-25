f61cf2835876316601d2853c450db369aece0ada
YUZJ <Zhejian.19@intl.zju.edu.cn>
2020-04-25 17:09:03 +0800

    Optimized Practical 11.

--- Practical11/24points.py
+++ Practical11/24points.py
@@ -11,7 +11,12 @@ for item in num:
         sys.exit(1)
 
 # List all possible situations
-def select_calc(l: int):
+def select_calc(l: int) -> list:
+    '''
+    This function is designed to list all the operations available with l cards. These operations are represented as [index of operation number 1, index of operation number 2. operation].
+    :param l: The number of cards.
+    :return: A list consisting all possible operation lists.
+    '''
     return_list = []
     for i in list(range(l - 1)):
         for j in list(range(i + 1, l)):
@@ -33,21 +38,6 @@ for i in range(len(num)):
 
 # Select one operation [] from one stage.
 
-def select_copy(l: list):
-    global all_sit
-    l_copy = l.copy()
-    if len(l_copy) == len(sci) - 1:
-        all_sit.append(l_copy)
-    for item in sci[len(l)]:
-        l.append(item)
-        select_copy(l)
-        l.pop()
-
-
-select_copy([])
-
-
-# Apply
 def select_apply(i: int, j: int, k: int, l: list):
     if k == 0:
         out = l[i] + l[j]
@@ -57,11 +47,16 @@ def select_apply(i: int, j: int, k: int, l: list):
         out = l[j] - l[i]
     elif (k == 3):
         out = l[i] * l[j]
-    elif (l[j] != 0):
-        if (k == 4):
+    elif (k == 4):
+        if(l[j] != 0):
             out = l[i] / l[j]
         else:
+            return (1)
+    else:
+        if (l[i] != 0):
             out = l[j] / l[i]
+        else:
+            return(1)
     if (out == 24):
         return (0)
     else:
@@ -69,16 +64,30 @@ def select_apply(i: int, j: int, k: int, l: list):
         l.pop(j)
         return (1)
 
+def select_copy(l: list):
+    '''
+    :param l: a list of all operation steps.
+    :return: NONE
+    '''
+    global all_sit
+    global n
+    l_copy = l.copy()
+    if len(l_copy) == len(sci) - 1:
+        num_copy = num.copy()
+        for jtem in l_copy:
+            if (select_apply(jtem[0], jtem[1], jtem[2], num_copy) == 0):
+                print("Yes")
+                print("Time: " + str(n))
+                sys.exit(0)
+            else:
+                n = n + 1
+    for item in sci[len(l)]:
+        l.append(item)
+        select_copy(l)
+        l.pop()
 
-n = 1
-for item in all_sit:
-    num_copy = num.copy()
-    for jtem in item:
-        if (select_apply(jtem[0], jtem[1], jtem[2], num_copy) == 0):
-            print("Yes")
-            print("Time: " + str(n))
-            sys.exit(0)
-        else:
-            n = n + 1
+# Apply
+n=1
+select_copy([])
 print("No")
 sys.exit(1)
