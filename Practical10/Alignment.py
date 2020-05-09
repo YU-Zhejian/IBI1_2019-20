82f5354276521746ce65be8de60380e21619c3e2
YUZJ <Zhejian.19@intl.zju.edu.cn>
2020-05-09 14:25:41 +0800

    Practical 10 fixed

--- Practical10/Alignment.py
+++ Practical10/Alignment.py
@@ -6,12 +6,10 @@ def getlargest(num1,num2):
     else:
         return(num2)  
 def trimstr(str1,num1):
-    while len(str1)<num1:
-        str1=" "+str1
-    return(str1)
+    return(" "*getlargest(num1-len(str1),0)+str1)
 # Main
 import os
-os.chdir(r"C:\Users\Admin\Documents\Smst2\IBI\IBI1_2019-20\Practical10")
+os.chdir("D:\\Smst2\\IBI1B\\IBI1_2019-20\\Practical10")
 # BLOSUM file retrived from https://www.ncbi.nlm.nih.gov/Class/FieldGuide/BLOSUM62.txt
 blos=open("BLOSUM62.txt","r")
 blos_all=blos.readlines()
@@ -26,20 +24,22 @@ for line in blos_all:
     else:
         code_matrix.append(line.strip().replace("  "," ").split(" ")[1:])
 # Read Seq
-seq1=open(input("The name of seq1>"),"r")
-seq2=open(input("The name of seq2>"),"r")
+seq1=open(input("File name of seq1>"),"r")
+seq2=open(input("File name of seq2>"),"r")
 seq1_all=""
 seq2_all=""
 for seq1_ss in seq1.readlines():
-    if not seq1_ss.strip().startswith(">"):
-        seq1_all+=seq1_ss.strip()
+    seq1_ss=seq1_ss.strip()
+    if not seq1_ss.startswith(">"):
+        seq1_all+=seq1_ss
     else:
-        seq1_name=seq1_ss[1:].strip()
+        seq1_name=seq1_ss[1:]
 for seq2_ss in seq2.readlines():
-    if not seq2_ss.strip().startswith(">"):
-        seq2_all+=seq2_ss.strip()
+    seq2_ss = seq2_ss.strip()
+    if not seq2_ss.startswith(">"):
+        seq2_all+=seq2_ss
     else:
-        seq2_name=seq2_ss[1:].strip()
+        seq2_name=seq2_ss[1:]
 # Alignment
 percentage=0
 score=0
@@ -56,14 +56,9 @@ for i in range(len(seq1_all)):
         seqdiff+="+"
     else:
         seqdiff+=" "
-# Trim name
-seqdiff_name="Alignment"
-lt=getlargest(getlargest(len(seq1_name),len(seq1_name)),len(seqdiff_name))
-seqdiff_name=trimstr(seqdiff_name,lt)
-seq1_name=trimstr(seq1_name,lt)
-seq2_name=trimstr(seq2_name,lt)
 # Print
-print(seq1_name+"/"+seq1_ss.strip())
-print(seqdiff_name+"/"+seqdiff)
-print(seq2_name+"/"+seq2_ss.strip())
+lt=getlargest(getlargest(len(seq1_name),len(seq1_name)),len("Alignment"))
+print(trimstr(seq1_name,lt)+": "+seq1_ss)
+print(trimstr("Alignment",lt)+": "+seqdiff)
+print(trimstr(seq2_name,lt)+": "+seq2_ss)
 print("Score: "+str(score)+" with percentage "+str(round(percentage/len(seq1_all)*100,2))+"%")
