1a40117b77a4929776828f37fd6ffcfff97e5dc4
YUZJ <Zhejian.19@intl.zju.edu.cn>
2020-05-09 14:39:54 +0800

    Practical 11 fixed

--- Practical11/24points.py
+++ Practical11/24points.py
@@ -1,6 +1,6 @@
 #!/usr/env python
 import sys
-
+import math
 raw_str = input(r"Please input numbers to compute 24: (use ',' to divide them)")
 num = []
 for item in raw_str.split(","):
@@ -9,7 +9,6 @@ for item in num:
     if ((item > 23) or (item < 1)):
         print("Invalid number " + str(item) + ", which should have been between integers 1 to 23.")
         sys.exit(1)
-
 # List all possible situations
 def select_calc(l: int) -> list:
     '''
@@ -23,37 +22,31 @@ def select_calc(l: int) -> list:
             for k in range(6):
                 return_list.append([i, j, k])
     return (return_list)
-
-
 sc = [[], []]
 for i in range(2, len(num) + 1):
     sc.append(select_calc(i))
-
 all_sit = []
 all_sit_tmp = []
 sci = []
 for i in range(len(num)):
     sci.append(sc[len(num) - i])
-
-
 # Select one operation [] from one stage.
-
 def select_apply(i: int, j: int, k: int, l: list):
     if k == 0:
         out = l[i] + l[j]
-    elif (k == 1):
+    elif k == 1:
         out = l[i] - l[j]
-    elif (k == 2):
+    elif k == 2:
         out = l[j] - l[i]
-    elif (k == 3):
+    elif k == 3:
         out = l[i] * l[j]
-    elif (k == 4):
+    elif k == 4:
         if(l[j] != 0):
             out = l[i] / l[j]
         else:
             return (1)
     else:
-        if (l[i] != 0):
+        if l[i] != 0:
             out = l[j] / l[i]
         else:
             return(1)
@@ -63,7 +56,6 @@ def select_apply(i: int, j: int, k: int, l: list):
         l[i] = out
         l.pop(j)
         return (1)
-
 def select_copy(l: list):
     '''
     :param l: a list of all operation steps.
@@ -77,7 +69,8 @@ def select_copy(l: list):
         for jtem in l_copy:
             if (select_apply(jtem[0], jtem[1], jtem[2], num_copy) == 0):
                 print("Yes")
-                print("Time: " + str(n))
+                l=len(num)
+                print("Recursion Times: " + str(n)+"/"+str((4**(l-1))*math.factorial(l)*(math.factorial(l-1))))
                 sys.exit(0)
             else:
                 n = n + 1
@@ -85,7 +78,6 @@ def select_copy(l: list):
         l.append(item)
         select_copy(l)
         l.pop()
-
 # Apply
 n=1
 select_copy([])
