d636fba3d5a080d3430d4dc4dec89297a12e9d98
YUZJ <Zhejian.19@intl.zju.edu.cn>
2020-05-14 15:50:04 +0800

    EOL fixed

--- Practical14/Assignment.py
+++ Practical14/Assignment.py
@@ -19,12 +19,11 @@ class afinfo:
         self.is_a=is_a
         self.dep=0
     def build_dep(self,my_list:List[__init__])->int:
-        self.dep = 0
-        for item in my_list:
-            if item.is_a.__contains__(self.id):
-                print(self,item)
-                self.dep=self.dep+item.build_dep(my_list)+1
-                break
+        if self.dep == 0:
+            for item in my_list:
+                if item.is_a.__contains__(self.id):
+                    item.build_dep(my_list)
+                    self.dep=self.dep+item.dep+1
         return self.dep
 af:List[afinfo]=[]
 af_all:List[afinfo]=[]
@@ -33,7 +32,7 @@ for item in terms:
     for is_a in item.getElementsByTagName('is_a'):
         tmpds.append(is_a.childNodes[0].data)
     tmp_af=afinfo(item.getElementsByTagName('id')[0].childNodes[0].data,item.getElementsByTagName('name')[0].childNodes[0].data,item.getElementsByTagName('def')[0].getElementsByTagName('defstr')[0].childNodes[0].data,tmpds)
-    if tmp_af.name.__contains__('autophagosome'):
+    if tmp_af.defstr.__contains__('autophagosome'):
         af.append(tmp_af)
     af_all.append(tmp_af)
 del DT, root, terms,tmpds,is_a
